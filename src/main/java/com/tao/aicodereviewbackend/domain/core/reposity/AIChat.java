4f351ae3a3527e517725ab5512024ef433feee56
GongTao <2662187002@qq.com>
2025-05-08 21:06:57 +0800

    测试

---
+++ src/main/java/com/tao/aicodereviewbackend/domain/core/reposity/AIChat.java
@@ -0,0 +1,13 @@
+package com.tao.aicodereviewbackend.domain.core.reposity;
+
+import dev.langchain4j.model.chat.request.ChatRequest;
+import dev.langchain4j.model.chat.response.ChatResponse;
+
+/**
+ * @author GongTao
+ * @create 2025-04-24 16:56
+ * 对接 AI 服务
+ */
+public interface AIChat {
+
+}
